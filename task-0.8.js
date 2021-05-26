6b5241065b159cd3cdbe7fcff98f687981b6ce67
Mpilo-code <76687110+Mpilo-code@users.noreply.github.com>
2021-05-26 11:52:22 +0200

    Created task file task-0.8.js and solved the task

---
+++ task-0.8.js
@@ -0,0 +1,30 @@
+function timeString(num) {
+    let hours = 0;
+    let minutes = 0;
+    let finalTime = "";
+
+    let convertToTime = num / 60;
+    hours = Math.floor(convertToTime); // this operation removes the decimal part of the value which are minutes
+    minutes = Math.floor((convertToTime - hours) * 60) // subtracting hours from the value * 60 produces minutes
+
+    if (hours > 1) {
+        finalTime = hours + " hours, ";
+    } else {
+        finalTime = hours + " hour, ";
+    }
+
+    if (minutes > 1) {
+
+        finalTime = finalTime + minutes + " minutes";
+
+    } else {
+
+        finalTime = finalTime + minutes + " minute";
+
+    }
+
+    console.log(finalTime);
+
+}
+
+console.log(timeString(133));
