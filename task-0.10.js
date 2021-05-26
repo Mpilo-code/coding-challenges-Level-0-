56c5af43babbe4dae6a9df053222962929ec1693
Mpilo-code <76687110+Mpilo-code@users.noreply.github.com>
2021-05-26 13:07:58 +0200

    Created task file task-0.10.js and solved the task

---
+++ task-0.10.js
@@ -0,0 +1,18 @@
+function commonChar(str1, str2) {
+    let common = "";
+    let str1LowerCase = str1.toLowerCase();
+    let str2LowerCase = str2.toLowerCase();
+
+    for (let i = 0; i < str1LowerCase.length; i++) {
+        for (let j = 0; j < str2LowerCase.length; j++) {
+            if (str1LowerCase[i].includes(str2LowerCase[j])) {
+                common += str2LowerCase[j] + ",";
+            }
+        }
+
+    }
+
+    console.log("Common letter: " + common);
+}
+
+console.log(commonChar("HOUSE", "computers"));
